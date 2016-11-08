2f55a418e479ef1522719fa9fc0df777c0d9955c
Karsten Gresch <git-2015@by-doing.org>
2016-11-08 23:12:50 +0100

    sendMessage implementation

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,6 +2,10 @@ var Chat = function(socket) {
   this.socket = socket;
 };
 
-Chat.prototype.sendMessage = function() {
-  
+Chat.prototype.sendMessage = function(room, text) {
+  var message = {
+    room: room,
+    text: text
+  };
+  this.socket.emit('message', message);
 };
