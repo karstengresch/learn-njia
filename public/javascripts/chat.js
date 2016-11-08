ca06b980e6e2f93da336ffe8a57ef00fe0b38344
Karsten Gresch <git-2015@by-doing.org>
2016-11-08 23:15:05 +0100

    changeRoom implementation

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -12,5 +12,7 @@ Chat.prototype.sendMessage = function(room, text) {
 
 
 Chat.prototype.changeRoom = function(room){
-  this.socket.emit();
+  this.socket.emit('join', {
+    newRoom: room
+  });
   };
