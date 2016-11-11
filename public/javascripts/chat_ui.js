0b57ca62dd558d78f34ff9fa0f9f8e8f5f6c2881
Karsten Gresch <git-2015@by-doing.org>
2016-11-11 23:32:18 +0100

    chat UI helper methods

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -2,3 +2,6 @@ function divEscapedContentElement(message) {
   return $('<div></div>').text(message);
 }
 
+function divSystemContentElement(message) {
+  return $('<div></div>').html('<i>' + message + '</i>');
+}
